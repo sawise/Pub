dc2d53708e94d0e8e77e8c53cae00c0224d7a578
Filip Fransson <fille_fransson@hotmail.com>
2013-11-12 12:43:48 +0100

    Ska merga ihop projecten...

---
+++ MyApplicationProject2/bottomApp/src/main/java/com/group2/bottomapp/DrinksCabinet.java
@@ -0,0 +1,32 @@
+package com.group2.bottomapp;
+
+/**
+ * Created by FilipFransson on 2013-11-12.
+ */
+
+    import android.os.Bundle;
+    import android.support.v4.app.Fragment;
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.view.ViewGroup;
+
+
+
+public class DrinksCabinet extends Fragment implements View.OnClickListener {
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.drinkscapinet, container, false);
+
+            return rootView;
+        }
+
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+}
---
+++ MyApplicationProject2/bottomApp/src/main/java/com/group2/bottomapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.group2.bottomapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends FragmentActivity {
+    public static final String POSITION = "POSITION";
+    final String[] menuTitle = {"Drinks cabinet", "Drinks"};
+    final String[] fragments = {
+            "com.group2.DrinksCabinet",
+            "com.group2.Drinks"
+    };
+    private int currentPos;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActionBar().getThemedContext(), android.R.layout.simple_list_item_1, menuTitle);
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById((R.id.drawerLayout));
+        final ListView navList = (ListView) findViewById(R.id.drawer);
+
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.replace(R.id.main, Fragment.instantiate(MainActivity.this, fragments[0]));
+        tx.commit();
+
+        navList.setAdapter(adapter);
+
+        navList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int pos, long id) {
+                currentPos = pos;
+                getActionBar().setTitle(menuTitle[pos]);
+                drawer.setDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        super.onDrawerClosed(drawerView);
+                        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+                        tx.replace(R.id.main, Fragment.instantiate(MainActivity.this, fragments[pos]));
+                        tx.commit();
+                    }
+                });
+                drawer.closeDrawer(navList);
+            }
+        });
+
+    }
+
+
+
+
+}
