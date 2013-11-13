4a74210b4da85babe8216d997cdb2340cc42abb5
Filip Fransson <fille_fransson@hotmail.com>
2013-11-13 12:39:30 +0100

    About

--- MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/About.java
+++ MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/About.java
@@ -1,7 +1,24 @@
 package com.group2.bottomapp;
 
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by FilipFransson on 2013-11-13.
  */
-public class About {
+public class About extends android.support.v4.app.Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.about, container, false);
+
+        return rootView;
+    }
+
+
+
 }
