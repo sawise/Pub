eaf11b567f4075103f1af4b267189c8b8e9fcfbf
Lukas Andersson <lukas_andersson@live.se>
2013-11-13 11:16:45 +0100

    Random

--- MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/Random.java
+++ MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/Random.java
@@ -1,7 +1,28 @@
 package com.group2.bottomapp;
 
-/**
- * Created by FilipFransson on 2013-11-13.
- */
-public class Random {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Random extends Fragment implements View.OnClickListener {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.random, container, false);
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
