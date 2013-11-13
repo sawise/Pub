87fba0ce2160dd1ce4ee631b71091be27f6f8d7d
Victor Olsson <kontakt@vicolsson.se>
2013-11-13 13:56:08 +0100

    Small fix

--- MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/addToCabinet.java
+++ MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/addToCabinet.java
@@ -25,9 +25,11 @@ public class addToCabinet extends Fragment implements View.OnClickListener {
         liquorList.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-                final String selected = (String) liquorListAdapter.getChild(
+                final String childString = (String) liquorListAdapter.getChild(
                         groupPosition, childPosition);
-                CabinetManager.AddIngredient(selected);
+                final String parentString = (String) liquorListAdapter.getGroup(groupPosition);
+
+                CabinetManager.AddIngredient(parentString + ", " + childString);
                 return true;
             }
         });
