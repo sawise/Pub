2ccf8d7620e1772b0cd7ad6bd8f952cf7784e76c
Lukas Andersson <lukas_andersson@live.se>
2013-11-13 12:37:34 +0100

    random layout

--- MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/Random.java
+++ MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/Random.java
@@ -5,16 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
 
 
 public class Random extends Fragment implements View.OnClickListener {
+    private ImageView ivDrinkImage;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.random, container, false);
+
+
+        ivDrinkImage = (ImageView) rootView.findViewById(R.id.ivDrinkImage);
+
 
         return rootView;
     }
