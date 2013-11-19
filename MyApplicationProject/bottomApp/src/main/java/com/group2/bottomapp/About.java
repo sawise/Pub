3f854f66dd4c1854e8b22fb3dde5a6a205fbd1c6
Lukas Andersson <lukas_andersson@live.se>
2013-11-19 11:27:39 +0100

    blää

--- MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/About.java
+++ MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/About.java
@@ -1,15 +1,14 @@
 package com.group2.bottomapp;
 
-
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by FilipFransson on 2013-11-13.
- */
-public class About extends android.support.v4.app.Fragment {
+
+public class About extends Fragment implements View.OnClickListener {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,5 +19,9 @@ public class About extends android.support.v4.app.Fragment {
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+    }
 
 }
