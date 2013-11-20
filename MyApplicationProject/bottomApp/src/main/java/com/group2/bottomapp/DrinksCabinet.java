fc6fdd25f1e77bc5472be5300905468046fee3f8
Sam Johnsson <johnsson.sam@gmail.com>
2013-11-20 15:16:03 +0100

    A dialog appears when clicking on capinetitems, items can be removed from dialog too

--- MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/DrinksCabinet.java
+++ MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/DrinksCabinet.java
@@ -1,6 +1,7 @@
 package com.group2.bottomapp;
 
     import android.app.Application;
+    import android.app.Dialog;
     import android.content.ClipData;
     import android.content.Intent;
     import android.graphics.Bitmap;
@@ -18,7 +19,10 @@ package com.group2.bottomapp;
     import android.widget.Adapter;
     import android.widget.AdapterView;
     import android.widget.ArrayAdapter;
+    import android.widget.Button;
     import android.widget.GridView;
+    import android.widget.ImageView;
+    import android.widget.TextView;
     import android.widget.Toast;
 
 
@@ -27,7 +31,7 @@ package com.group2.bottomapp;
     import java.util.List;
 
 
-public class DrinksCabinet extends Fragment implements View.OnClickListener, GridView.OnItemClickListener {
+public class DrinksCabinet extends Fragment implements View.OnClickListener, GridView.OnItemClickListener, GridView.OnItemLongClickListener {
 
 
     //public ArrayList<HashMap<Integer, String>> drinkListt = new ArrayList<HashMap<Integer, String>>();
@@ -35,12 +39,17 @@ public class DrinksCabinet extends Fragment implements View.OnClickListener, Gri
     private  ArrayList<Cocktail> gridArray = new ArrayList<Cocktail>();
     private CustomGridViewAdapter customGridAdapter;
     private CabinetManager cabinetManager;
-    private Application app;
+    private int selectedItem;
     private String[] testStr = {"Vodka",  "Beer", "Mulled wine"};
     private Integer[] testInt = {R.drawable.bottle_one, R.drawable.bottle_two, R.drawable.bottle_three};
+    TextView dialogText;
+    TextView alcoholrate;
+    ImageView dialogImage;
+    Button removeButton;
 
 
-        @Override
+
+    @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.drinkscapinet, container, false);
@@ -50,7 +59,9 @@ public class DrinksCabinet extends Fragment implements View.OnClickListener, Gri
 
             customGridAdapter = new CustomGridViewAdapter(this.getActivity(), R.layout.row_grid, gridArray);
             drinkGridView.setAdapter(customGridAdapter);
+            drinkGridView.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE);
             drinkGridView.setOnItemClickListener(this);
+            drinkGridView.setOnItemLongClickListener(this);
 
 
             for(int i = 0; i<=20; i++){
@@ -76,7 +87,11 @@ public class DrinksCabinet extends Fragment implements View.OnClickListener, Gri
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         if(parent == drinkGridView){
-            Toast.makeText(getActivity(), gridArray.get(position).getName(), Toast.LENGTH_LONG).show();
+            selectedItem = position;
+            String text = gridArray.get(selectedItem).getName();
+            int imageId = gridArray.get(selectedItem).getImageId();
+            dialog(text,imageId);
+            //Toast.makeText(getActivity(), gridArray.get(position).getName(), Toast.LENGTH_SHORT).show();
         }
     }
        @Override
@@ -99,5 +114,42 @@ public class DrinksCabinet extends Fragment implements View.OnClickListener, Gri
                 break;
         }
         return false;
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if(adapterView == drinkGridView){
+            Log.i("gridview", l + "<->" + i);
+
+        }
+        return false;
+    }
+
+    public void dialog(String text, int imageID){
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.drinkcabinetdialog);
+        dialog.setTitle(text);
+        dialog.setCancelable(true);
+
+        dialogText = (TextView) dialog.findViewById(R.id.dialogText);
+        alcoholrate = (TextView) dialog.findViewById(R.id.alcoholrate);
+        dialogImage = (ImageView) dialog.findViewById(R.id.dialogImage);
+        removeButton = (Button) dialog.findViewById(R.id.buttonRemove);
+
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("gridarray remove", selectedItem+"");
+                gridArray.remove(selectedItem);
+                customGridAdapter.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+        });
+
+        dialogText.setText(text);
+        alcoholrate.setText("99%");
+        dialogImage.setImageResource(imageID);
+
+        dialog.show();
+
     }
 }
