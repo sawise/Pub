c20baf1a742865afaaed1e2514bea6c3eeef05ca
Lukas Andersson <lukas_andersson@live.se>
2013-11-27 14:41:18 +0100

    add Favorites.java/xml delete hejhej

---
+++ MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/Favorites.java
@@ -0,0 +1,30 @@
+package com.group2.bottomapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+
+public class Favorites extends Fragment implements View.OnClickListener {
+
+    private ListView favList;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.favorites, container, false);
+
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+}
