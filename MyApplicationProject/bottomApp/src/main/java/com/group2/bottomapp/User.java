a17ca034ddded3192abdb0968b4e2af5d3b1ded4
Lukas Andersson <lukas_andersson@live.se>
2013-12-03 12:36:14 +0100

    users, mail on ice with scooby doo

---
+++ MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/User.java
@@ -0,0 +1,50 @@
+package com.group2.bottomapp;
+
+
+public class User {
+    public int id;
+    public String username;
+    public String email;
+    public String password;
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public void setUsername(String Username) {
+        this.username = username;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
