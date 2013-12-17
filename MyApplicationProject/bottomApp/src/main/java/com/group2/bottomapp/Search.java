4384cc5ed1a890e501b1211dd22332d903ce1a2c
Lukas Andersson <lukas_andersson@live.se>
2013-12-17 14:08:45 +0100

    Sök på g

---
+++ MyApplicationProject/bottomApp/src/main/java/com/group2/bottomapp/Search.java
@@ -0,0 +1,56 @@
+package com.group2.bottomapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+/**
+ * Created by Lukas on 2013-12-17.
+ */
+public class Search extends Activity implements SearchView.OnQueryTextListener {
+
+    private SearchView mSearchView;
+    private ListView mListView;
+
+    private final String[] mStrings = LiquorSearch.sCheeseStrings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+
+        setContentView(R.layout.search_layout);
+
+        mSearchView = (SearchView) findViewById(R.id.search_view);
+        mListView = (ListView) findViewById(R.id.list_view);
+        mListView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                mStrings));
+        mListView.setTextFilterEnabled(true);
+        setupSearchView();
+    }
+
+    private void setupSearchView() {
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setSubmitButtonEnabled(true);
+        mSearchView.setQueryHint("Search Here");
+    }
+
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            mListView.clearTextFilter();
+        } else {
+            mListView.setFilterText(newText.toString());
+        }
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+}
